87363e1b74d7bcefce85cbd8ac2d3c844362ac82
Paul Draper <paulddraper@gmail.com>
2022-01-13 10:21:21 -0700

    Fix gen_bazelrc

--- tools/gen-bazelrc.sh
+++ tools/gen-bazelrc.sh
@@ -7,9 +7,15 @@ function escape_pattern {
   <<< "$1" sed 's/[\/&]/\\&/g'
 }
 
-packages="$( \
+file_packages="$( \
     (find . -name BUILD -o -name BUILD.bazel) \
     | sed -e 's:/[^/]*$::' -e 's:^\./::' -e 's:^:@rules_format_files//files/:' -e 's:/.$::' \
 )"
 
-sed -e "s/%{deleted_packages}/$(escape_pattern "$packages")/g" "$RUNFILES_DIR/rules_format/tools/deleted.bazelrc.template" > tools/deleted.bazelrc
+packages="$(
+    (echo "$file_packages") \
+    | tr '\n' , \
+    | sed 's/,$//' \
+)"
+
+sed -e "s/%{deleted_packages}/$(escape_pattern "$packages")/g" "$RUNFILES_DIR/rules_format/tools/deleted.bazelrc.tpl" > tools/deleted.bazelrc
