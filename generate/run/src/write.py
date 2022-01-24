83d7f9023484218571fb47f1af846dd94de3802d
Paul Draper <paulddraper@gmail.com>
2022-01-24 08:17:51 -0700

    Fix generate write for directory

--- generate/run/src/write.py
+++ generate/run/src/write.py
@@ -39,11 +39,11 @@ def main(args):
         if stat.S_ISDIR(out_stat.st_mode):
             shutil.copytree(out, src, copy_function=shutil.copy)
             os.chmod(src, args.dir_mode)
-            for path, dirnames, filenames in os.walk(path):
-                for dirname in dirname:
-                    os.chmod(os.path.join(path, dirname), args.dir_mode)
-                for filename in filename:
-                    os.chmod(os.path.join(path, filename), args.file_mode)
+            for dirpath, dirnames, filenames in os.walk(src):
+                for dirname in dirnames:
+                    os.chmod(os.path.join(dirpath, dirname), args.dir_mode)
+                for filename in filenames:
+                    os.chmod(os.path.join(dirpath, filename), args.file_mode)
         else:
             shutil.copy(out, src)
             os.chmod(src, args.file_mode)
